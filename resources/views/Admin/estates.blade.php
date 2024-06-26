1178949469bc9d7d7a48b1a97f21bccbbba0f503
huzaifa137 <huzaifabukenya227@gmail.com>
2024-06-26 11:33:59 +0300

    Live-modification v.0.0.1

--- resources/views/Admin/estates.blade.php
+++ resources/views/Admin/estates.blade.php
@@ -73,44 +73,100 @@
 
             </div>
 
-            <div class="row ">
-              <div class="col-12 grid-margin">
-                <div class="card">
-                  <div class="card-body">
-                    <h4 class="card-title">All Estates </h4>
-                    <div class="table-responsive">
-                      <table class="table">
+            <!--<div class="row ">-->
+            <!--  <div class="col-12 grid-margin">-->
+            <!--    <div class="card">-->
+            <!--      <div class="card-body">-->
+            <!--        <h4 class="card-title">All Estates </h4>-->
+            <!--        <div class="table-responsive">-->
+            <!--          <table class="table">-->
+            <!--            <thead>-->
+            <!--              <tr>-->
+            <!--                <th>No</th>-->
+            <!--                <th> Estate Name</th>-->
+            <!--                <th> Estate Price </th>-->
+            <!--                <th> Location </th>-->
+            <!--                <th> Number of plots </th>-->
+            <!--                <th>View Estate</th>-->
+            <!--              </tr>-->
+            <!--            </thead>-->
+            <!--            <tbody>-->
+                        
+            <!--          @foreach ($estates as $key => $item)-->
+                        
+            <!--            <tr>-->
+            <!--                <td><a href="{{'view-estate/'.$item->id}}"> {{$key+1}} </a></td>-->
+            <!--                <td><a href="{{'view-estate/'.$item->id}}"> {{$item->estate_name}} </a></td>-->
+            <!--                <td><a href="{{'view-estate/'.$item->id}}"> {{$item->estate_price}} </a></td>-->
+            <!--                <td><a href="{{'view-estate/'.$item->id}}"> {{$item->location}} </a></td>-->
+            <!--                <td><a href="{{'view-estate/'.$item->id}}"> {{$item->number_of_plots}} </a></td>-->
+            <!--                <td><a href="{{'view-estate/'.$item->id}}"><i class="btn btn-success">View Estate</i></a></td>-->
+            <!--                {{-- <td><a href="{{'delete/'.$info->id}}" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure you want to delete this case ?')"><i class="la la-trash-o"></i></a></td> --}}-->
+            <!--              </tr>-->
+            <!--              @endforeach    -->
+            <!--            </tbody>-->
+            <!--          </table>-->
+            <!--        </div>-->
+            <!--      </div>-->
+            <!--    </div>-->
+            <!--  </div>-->
+            <!--</div>-->
+            
+            <div class="row">
+    <div class="col-12 grid-margin">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">All Estates</h4>
+                <!-- Search Bar -->
+                <input type="text" id="searchBar" placeholder="Search estates by name..." class="form-control mb-4">
+
+                <div class="table-responsive">
+                    <table class="table" id="estatesTable">
                         <thead>
-                          <tr>
-                            <th>No</th>
-                            <th> Estate Name</th>
-                            <th> Estate Price </th>
-                            <th> Location </th>
-                            <th> Number of plots </th>
-                            <th>View Estate</th>
-                          </tr>
+                            <tr>
+                                <th>No</th>
+                                <th>Estate Name</th>
+                                <th>Estate Price</th>
+                                <th>Location</th>
+                                <th>Number of plots</th>
+                                <th>View Estate</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        
-                      @foreach ($estates as $key => $item)
-                        
-                        <tr>
-                            <td><a href="{{'view-estate/'.$item->id}}"> {{$key+1}} </a></td>
-                            <td><a href="{{'view-estate/'.$item->id}}"> {{$item->estate_name}} </a></td>
-                            <td><a href="{{'view-estate/'.$item->id}}"> {{$item->estate_price}} </a></td>
-                            <td><a href="{{'view-estate/'.$item->id}}"> {{$item->location}} </a></td>
-                            <td><a href="{{'view-estate/'.$item->id}}"> {{$item->number_of_plots}} </a></td>
-                            <td><a href="{{'view-estate/'.$item->id}}"><i class="btn btn-success">View Estate</i></a></td>
-                            {{-- <td><a href="{{'delete/'.$info->id}}" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure you want to delete this case ?')"><i class="la la-trash-o"></i></a></td> --}}
-                          </tr>
-                          @endforeach    
+                            @foreach ($estates as $key => $item)
+                                <tr>
+                                    <td><a href="{{ url('view-estate/'.$item->id) }}"> {{$key+1}} </a></td>
+                                    <td><a href="{{ url('view-estate/'.$item->id) }}"> {{$item->estate_name}} </a></td>
+                                    <td><a href="{{ url('view-estate/'.$item->id) }}"> {{$item->estate_price}} </a></td>
+                                    <td><a href="{{ url('view-estate/'.$item->id) }}"> {{$item->location}} </a></td>
+                                    <td><a href="{{ url('view-estate/'.$item->id) }}"> {{$item->number_of_plots}} </a></td>
+                                    <td><a href="{{ url('view-estate/'.$item->id) }}" class="btn btn-success">View Estate</a></td>
+                                </tr>
+                            @endforeach
                         </tbody>
-                      </table>
-                    </div>
-                  </div>
+                    </table>
                 </div>
-              </div>
             </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    document.getElementById('searchBar').addEventListener('keyup', function() {
+        const query = this.value.toLowerCase();
+        const rows = document.querySelectorAll('#estatesTable tbody tr');
+
+        rows.forEach(row => {
+            const estateName = row.querySelector('td:nth-child(2) a').textContent.toLowerCase();
+            if (estateName.includes(query)) {
+                row.style.display = '';
+            } else {
+                row.style.display = 'none';
+            }
+        });
+    });
+</script>
+
 
           
           </div>
